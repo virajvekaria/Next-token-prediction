b743d1ca4611dc9be89b69a22db7b204b898675c
Viraj Vekaria <virajv5593@gmail.com>
2024-03-23 13:29:39 +0530

    Adding relevant details

--- next_token_app.py
+++ next_token_app.py
@@ -3,8 +3,8 @@ import torch
 import torch.nn as nn
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-seed = st.number_input("Random Seed", min_value=1, value=100)
 
+# Generating the next characters with the current model by passing the context size, like sliding window of sorts
 def generate_next_chars(model, stoi, itos, context, k):
     context = [stoi[ch] for ch in context]
 
@@ -22,12 +22,15 @@ def generate_next_chars(model, stoi, itos, context, k):
 
 st.title("Next Token Prediction")
 
+# Inputting values from the users in the app
 emb_size = st.slider("Embedding Size", 2, 6, value=4)
 block_size = st.slider("Block Size", 5, 10, value=7)
 model_num = st.slider("Model Number", 1, 5, value =3)
 context = st.text_input("Context String", value="brutus:", max_chars=block_size)
+seed = st.number_input("Random Seed", min_value=1, value=100)
 k = st.number_input("Number of Characters to Generate", min_value=1, value=100)
 
+# Defining all the models, so as to load the pre-trained models' .pth files from /models
 if(model_num == 1):
     class NextChar(nn.Module):
         def __init__(self, block_size, vocab_size, emb_dim, hidden_size):
@@ -108,6 +111,7 @@ elif(model_num == 4):
 
             return x
 
+# Defining the size and then creating a NextChar instance as defined by the model_num
 def load_model(emb_size, block_size, model_num, vocab_size):
     model_path = f"models/model_{emb_size}_{block_size}_{model_num}.pth"
     if model_num == 1: 
@@ -135,7 +139,7 @@ def load_model(emb_size, block_size, model_num, vocab_size):
     model.eval()
     return model
 
-
+# Generate but to generate next characters as the button is pressed
 if st.button("Generate"):
     itos = {0: ' ', 1: '!', 2: '&', 3: "'", 4: ',', 5: '-', 6: '.', 7: ':', 8: ';', 9: '?', 10: 'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f', 16: 'g', 17: 'h', 18: 'i', 19: 'j', 20: 'k', 21: 'l', 22: 'm', 23: 'n', 24: 'o', 25: 'p', 26: 'q', 27: 'r', 28: 's', 29: 't', 30: 'u', 31: 'v', 32: 'w', 33: 'x', 34: 'y', 35: 'z'}
     stoi = {i:s for s,i in itos.items()}
@@ -144,4 +148,4 @@ if st.button("Generate"):
 
     next_chars = generate_next_chars(model, stoi, itos, context, k)
     st.write("Next characters:")
-    st.write(next_chars
+    st.write(next_chars)
